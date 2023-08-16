e36c7853e92e0ff5b880aab58d9b8118b3cb8b07
cristian <caafanadord@ufpso.edu.co>
2023-08-16 10:56:30 -0500

    firts Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+/**
+ * Importación type: Module / ES
+ */
+ import express, {Router} from 'express';
+ import bodyParser from 'body-parser';
+
+/**  
+ * Importación Normal, remover del package.json "type": "module"
+ * const express  = require("express");
+ * const { Router }   = require("express");
+ * const bodyParser  = require("body-parser");
+*/
+
+/** Inicializadores  */
+const app = express();
+const router = Router();
+
+/** Middleware */
+app.use(bodyParser.json());
+
+
+/** Routing  */
+
+/**
+ * Las rutas se pueden utilizar de esta manera pero:
+ * esta ruta acepta todas las peticiones tanto como 
+ * GET, POST, PUT, DELETE, ETC...
+ */
+app.use('/usuario', (req, res)=>{
+    res.status(200).send({success:true });
+}) 
+
+
+/**
+ * Rutas controladas 
+ */
+app.use( '/producto' ,router.get( '',(req, res)=> {
+    res.status(200).send({success:true })
+}))
+
+app.use(router.post('/producto', (req, res)=> {
+    const { nombre } = req.body
+    console.log(nombre);
+    res.status(200).send({success:true })
+}))
+
+const port = 4200;
+/**
+    Inicializa el servidor 
+    @param port: Puerto donde esta estara habilidato el servidor
+    @function callback: Función que se ejecuta despues de ejecutar el servidor
+*/
+app.listen(port, ()=> {console.log("server");})
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Clase 15-08-2023
+## Instalación
+
+### Clonar el repositorio:
+```
+    git clone https://github.com/bluemaster23/clase.git
+    cd clase
+```
+
+### Instalación Manual
+
+```
+    npm install
+```
+
+## Tabla de contenido
+
+### Característica
+- Node js
+- NPM
+
+Run Local:
+```
+    npm run dev
+```
+Run Producción:
+```
+    npm run start
+```
+
+## API Endpoints
+
+<code>GET, POST, PUT, DELETE /usuario</code> - Petición básica de enrutado
+
+<code>GET /producto</code> - Petición solo GET
+
+<code>POST /producto</code>- Petición solo POST
